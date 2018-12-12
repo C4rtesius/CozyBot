f93c2bd0fff93a5760dcfae76021f2e29edbed6f
C4rtesius <cartesius0x7ca@gmail.com>
2018-12-12 18:36:37 +0200

    Changed emoji codes to winter emojis' ones.

--- CozyBot/EmojiCodes.cs
+++ CozyBot/EmojiCodes.cs
@@ -6,14 +6,14 @@ namespace DiscordBot1
 {
     public static class EmojiCodes
     {
-        public static string Picardia => @"<:picardia:406596128017088516>";
+        public static string Picardia => @"<:picardia:518215733583413249>";
         public static string Pizdec => @"<:pizdec:334783018549903376>";
-        public static string Pepe => @"<:pepe:406595525052334110>";
-        public static string Thonk => @"<:thonk:406595630790737922>";
-        public static string BobRoss => @"<:bobross:447441960173830164>";
-        public static string DankPepe => @"<:dankpepe:483597449751953420>";
-        public static string Bumagi => @"<:bumagi:406596475649261569>";
-        public static string WaitWhat => @"<:waitwhat:406595558346719243>";
-        public static string Tomas => @"<:tomas:406596388554670092>";
+        public static string Pepe => @"<:pepe:518215109261393920>";
+        public static string Thonk => @"<:thonk:518215197912072212>";
+        public static string BobRoss => @"<:bobross:518216231694762012>";
+        public static string DankPepe => @"<:dankpepe:518214729219571755>";
+        public static string Bumagi => @"<:bumagi:518216004229398528>";
+        public static string WaitWhat => @"<:waitwhat:518215162457751588>";
+        public static string Tomas => @"<:tomas:518215918065811466>";
     }
 }
