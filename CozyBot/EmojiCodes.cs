1a52b09d3150e6c0ee5058d35e86efa36185de24
C4rtesius <cartesius0x7ca@gmail.com>
2019-01-31 17:31:39 +0200

    Changed emoji from winter ones to usual. Updated to Discord.Net 2.0.1 .

--- CozyBot/EmojiCodes.cs
+++ CozyBot/EmojiCodes.cs
@@ -6,14 +6,14 @@ namespace DiscordBot1
 {
     public static class EmojiCodes
     {
-        public static string Picardia => @"<:picardia:518215733583413249>";
+        public static string Picardia => @"<:picardia:540541064969191445>";
         public static string Pizdec => @"<:pizdec:334783018549903376>";
-        public static string Pepe => @"<:pepe:518215109261393920>";
-        public static string Thonk => @"<:thonk:518215197912072212>";
-        public static string BobRoss => @"<:bobross:518216231694762012>";
-        public static string DankPepe => @"<:dankpepe:518214729219571755>";
-        public static string Bumagi => @"<:bumagi:518216004229398528>";
-        public static string WaitWhat => @"<:waitwhat:518215162457751588>";
-        public static string Tomas => @"<:tomas:518215918065811466>";
+        public static string Pepe => @"<:pepe:540540414793482250>";
+        public static string Thonk => @"<:thonk:540540493088423938>";
+        public static string BobRoss => @"<:bobross:540540035729195025>";
+        public static string DankPepe => @"<:dankpepe:540540295713128449>";
+        public static string Bumagi => @"<:bumagi:540540114888163338>";
+        public static string WaitWhat => @"<:waitwhat:540540454559547422>";
+        public static string Tomas => @"<:tomas:540540188632416256>";
     }
 }
