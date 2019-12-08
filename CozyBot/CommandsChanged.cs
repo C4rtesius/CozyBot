80a6ff794bf6794dd875713882192e67d1931690
C4rtesius <cartesius0x7ca@gmail.com>
2019-12-08 05:42:21 +0200

    Removed unused files.

--- CozyBot/CommandsChanged.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace CozyBot
-{
-    public delegate void CommandsChanged(object sender, CommandsChangedArgs args);
-}
--- CozyBot/ModuleLogged.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace CozyBot
-{
-    public delegate void ModuleLogged(object sender, ModuleLoggedArgs args);
-}
