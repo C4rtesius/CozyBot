ad5e6d9147ca304923672c1d6699235582fd6887
Roman Dubrovka <cartesius0x7ca@gmail.com>
2021-11-13 08:35:22 +0200

    CtrlEvent, Emoji and Guard cleanup.

--- CozyBot/CtrlType.cs
+++ CozyBot/CtrlType.cs
@@ -1,15 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
-
 namespace CozyBot
 {
-    public enum CtrlType
-    {
-        CTRL_C_EVENT = 0,
-        CTRL_BREAK_EVENT = 1,
-        CTRL_CLOSE_EVENT = 2,
-        CTRL_LOGOFF_EVENT = 5,
-        CTRL_SHUTDOWN_EVENT = 6
-    }
+  public enum CtrlType
+  {
+    CTRL_C_EVENT = 0,
+    CTRL_BREAK_EVENT = 1,
+    CTRL_CLOSE_EVENT = 2,
+    CTRL_LOGOFF_EVENT = 5,
+    CTRL_SHUTDOWN_EVENT = 6
+  }
 }
--- CozyBot/Guard.cs
+++ CozyBot/Guard.cs
@@ -1,23 +1,18 @@
 using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace CozyBot
 {
-    public static class Guard
-    {
-        public static string NonNullWhitespaceEmpty(string value, string paramName)
-            =>
-                (String.IsNullOrEmpty(value) || String.IsNullOrWhiteSpace(value))
-                ? throw new ArgumentException($"{paramName} cannot be null, whitespace or empty.", paramName)
-                : value;
+  public static class Guard
+  {
+    public static string NonNullWhitespaceEmpty(string value, string paramName)
+      => String.IsNullOrEmpty(value) || String.IsNullOrWhiteSpace(value) ?
+         throw new ArgumentException($"{paramName} cannot be null, whitespace or empty.", paramName) :
+         value;
 
+    public static T NonNull<T>(T obj, string paramName) where T : class
+      => obj ?? throw new ArgumentNullException(paramName, $"{paramName} cannot be null.");
 
-        public static T NonNull<T>(T obj, string paramName)
-            where T : class
-            => obj ?? throw new ArgumentNullException(paramName, $"{paramName} cannot be null.");
-
-        public static int Positive(int value, string paramName)
-            => (value > 0) ? value : throw new ArgumentException($"{paramName} must be greater than zero.", paramName);
-    }
+    public static int Positive(int value, string paramName)
+      => (value > 0) ? value : throw new ArgumentException($"{paramName} must be greater than zero.", paramName);
+  }
 }
