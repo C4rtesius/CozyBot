f52363d1b9f707d6763b73908a29c0de67c41716
Roman Dubrovka <cartesius0x7ca@gmail.com>
2021-11-13 03:55:36 +0200

    CanvasData cleanup.

--- CozyBot/CanvasData.cs
+++ CozyBot/CanvasData.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 
 using SixLabors.ImageSharp;
@@ -6,13 +5,12 @@ using SixLabors.ImageSharp.PixelFormats;
 
 namespace CozyBot
 {
-    struct CanvasData
-    {
-        public int Width;
-        public int Height;
-        public Image<Rgba32> Canvas;
-        public Dictionary<byte, Rgba32> Palette;
-
-        public Rgba32 this[int x, int y] => Canvas[x, y];
-    }
+  struct CanvasData
+  {
+    public int Width;
+    public int Height;
+    public Image<Rgba32> Canvas;
+    public Dictionary<byte, Rgba32> Palette;
+    public Rgba32 this[int x, int y] => Canvas[x, y];
+  }
 }
