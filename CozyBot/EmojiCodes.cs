4f2d948e2936f8d1f92ea16500c8879109ff7c2c
Roman Dubrovka <cartesius0x7ca@gmail.com>
2021-12-09 16:50:22 +0200

    Updated emoji codes.

--- CozyBot/EmojiCodes.cs
+++ CozyBot/EmojiCodes.cs
@@ -2,14 +2,14 @@ namespace CozyBot
 {
   public static class EmojiCodes
   {
-    public static string Picardia => @"<:picardia:816282138781417492>";
-    public static string Pizdec   =>   @"<:pizdec:816282841188532266>";
-    public static string Pepe     =>     @"<:pepe:816282073999867935>";
-    public static string Thonk    =>    @"<:thonk:816303764365246484>";
-    public static string BobRoss  =>  @"<:bobross:816282021339987970>";
-    public static string DankPepe => @"<:dankpepe:816281732738187314>";
+    public static string Picardia => @"<:picardia:915367713390600212>";
+    public static string Pizdec   =>   @"<:pizdec:915367411371364452>";
+    public static string Pepe     =>     @"<:pepe:915367986020380722>";
+    public static string Thonk    =>    @"<:thonk:915368908649816146>";
+    public static string BobRoss  =>  @"<:bobross:915367826141880390>";
+    public static string DankPepe => @"<:dankpepe:915368011291054090>";
     public static string Bumagi   =>   @"<:bumagi:580709087243796480>";
-    public static string WaitWhat => @"<:waitwhat:816283276935168030>";
-    public static string Tomas    =>    @"<:tomas:816282310289915904>";
+    public static string WaitWhat => @"<:waitwhat:915525353328500756>";
+    public static string Tomas    =>    @"<:tomas:915368615426019389>";
   }
 }
